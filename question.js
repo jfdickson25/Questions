80e74bd719856bf736336a317961032af3520aed
Jordan Dickson <jfdickson25@gmail.com>
2021-06-17 21:01:33 -0600

    added link to add question

--- question.js
+++ question.js
@@ -6,29 +6,6 @@ document.querySelector('.questionBtn').addEventListener('click', () => {
     });
 });
 
-document.querySelector('.addQuestionBtn').addEventListener('click', () => {
-    const question = {
-        question: document.querySelector('#question').value
-    }
-
-    fetch('https://example.com/profile', {
-        method: 'POST', // or 'PUT'
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(question),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        document.querySelector('#question').value = '';
-    })
-    .catch((error) => {
-    console.error('Error:', error);
-    });
-
-});
-
 
 
 function getFirstQuestion() {
